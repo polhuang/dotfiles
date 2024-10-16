9d55c1fdd4aae312960574957708d4e86aa8ab88
Paul Huang <paulleehuang@gmail.com>
2024-10-16 13:03:43 -0500

    Add services to core-system

--- polterguix/base.scm
+++ polterguix/base.scm
@@ -1,8 +1,7 @@
-(define-module (polhuang systems base)
+(define-module (polterguix systems core-system)
   #:use-module (gnu)
   #:use-module (gnu system)
-  #:use-module (nongnu packages linux)
-  #:export (system-config))
+  #:use-module (nongnu packages linux))
   
 (use-service-modules guix admin sysctl pm nix avahi dbus cups desktop linux
 		     mcron networking xorg ssh docker audio virtualization)
@@ -13,7 +12,6 @@
   
 (define-public base-operating-system
   (operating-system
-   (host-name "akhetaten")
    (timezone "America/Chicago")
    (locale "en_US.utf8")
    
@@ -40,7 +38,7 @@
    
    ;; users
    (users (cons (user-account
-                 (name "polhuang")
+                 (name "pol")
                  (comment "Paul Huang")
                  (group "users")
                  (home-directory "/home/polhuang")
@@ -74,67 +72,3 @@
    (services
     (cons* (service bluetooth-service-type)
            %base-services))))
-
-(define* (system-config #:key system home)
-  (operating-system
-     (host-name "akhetaten")
-     (timezone "America/Chicago")
-     (locale "en_US.utf8")
-     
-     ;; Use non-free Linux and firmware
-     (kernel linux)
-     (firmware (list linux-firmware))
-     
-     (keyboard-layout (keyboard-layout "us"))
-     
-     ;; uefi grub with efi
-     (bootloader (bootloader-configuration
-                  (bootloader grub-efi-bootloader)
-                  (targets '("/boot/efi"))
-                  (keyboard-layout keyboard-layout)))
-     
-     ;; placeholder file-system
-     (file-systems (cons*
-                    (file-system
-                     (mount-point "/tmp")
-                     (device "none")
-                     (type "tmpfs")
-                     (check? #f))
-                    %base-file-systems))
-     
-     ;; users
-     (users (cons (user-account
-                   (name "polhuang")
-                   (comment "Paul Huang")
-                   (group "users")
-                   (home-directory "/home/polhuang")
-                  (supplementary-groups '("wheel"  ;; sudo
-                                          "netdev" ;; network devices
-                                          "kvm"
-                                          "tty"
-                                          "input"
-                                          "docker"
-                                          "realtime" ;; realtime scheduling
-                                          "lp"       ;; bluetooth devices
-                                          "audio"    ;; audio devices
-                                          "video"))) ;; video devices
-                  %base-user-accounts))
-     
-     ;; Install bare-minimum system packages
-     (packages (cons* bluez
-                      bluez-alsa
-                      brightnessctl
-                      emacs-no-x-toolkit
-                      exfat-utils
-                      fuse-exfat
-                      git
-                      gvfs
-                      libva-utils
-                      ntfs-3g
-                      stow
-                      vim
-                      %base-packages))
-
-     (services
-      (cons* (service bluetooth-service-type)
-             %base-services))))
