e702ab20c034bdff106ee3378841bbc0edcbdd56
Paul Huang <paulleehuang@gmail.com>
2024-10-17 08:13:57 -0500

    Finish core-system - working

--- polterguix/systems/core-system.scm
+++ polterguix/systems/core-system.scm
@@ -1,22 +1,45 @@
 (define-module (polterguix systems core-system)
   #:use-module (gnu)
   #:use-module (gnu system)
-  #:use-module (nongnu system linux-initrd)
-  #:use-module (gnu packages emacs-xyz)
-  #:use-module (polterguix packages hyprland)
   #:use-module (nongnu packages linux)
-  #:use-module (rosenthal packages wm))
+  #:use-module (nongnu system linux-initrd)
+  #:use-module (gnu packages linux)
+  
+  #:use-module (gnu system privilege)
+  #:use-module (gnu system setuid)
+  #:use-module (gnu packages emacs)
+  #:use-module (gnu packages emacs-xyz)
+  #:use-module (gnu packages file-systems)
+  #:use-module (gnu packages audio)
+  #:use-module (gnu packages version-control)
+  #:use-module (gnu packages gnome)
+  #:use-module (gnu packages video)
+  #:use-module (gnu packages package-management)
+  #:use-module (gnu packages vim)
+  #:use-module (gnu packages shells)
+  #:use-module (gnu packages fonts)
+  #:use-module (gnu packages ssh)
+  #:use-module (gnu packages cups)
+  #:use-module (gnu packages libusb)
+  #:use-module (polterguix packages hyprland)
+  #:use-module (rosenthal packages wm)
+  
+  #:use-module (gnu services desktop)
+  #:use-module (gnu services networking)
+  #:use-module (gnu services avahi)
+  #:use-module (gnu services dbus)
+  #:use-module (gnu services pm)
+  #:use-module (gnu services linux)
+  #:use-module (gnu services docker)
+  #:use-module (gnu services virtualization)
+  #:use-module (gnu services ssh)
+  #:use-module (gnu services cups)
+  #:use-module (gnu services nix)
+  #:use-module (gnu services mcron))
 
-(use-service-modules guix admin sysctl pm nix avahi dbus cups desktop linux
-		     mcron networking xorg ssh docker audio virtualization)
-
-(use-package-modules audio video nfs certs shells ssh linux bash emacs gnome
-		     networking wm fonts libusb cups freedesktop file-systems
-		     version-control package-management vim)
-
-(define-public base-operating-system
+(define-public core-operating-system
   (operating-system
-   (host-name "hackstock")
+   (host-name "core")
    (kernel linux)
    (firmware (list linux-firmware))
    (initrd microcode-initrd)
@@ -94,10 +117,110 @@
                                                   (terminal-vt "1")
                                                   (terminal-switch #t))
                                                  (greetd-terminal-configuration (terminal-vt "2"))
-                                                 (greetd-terminal-configuration (terminal-vt "3")))))))
+                                                 (greetd-terminal-configuration (terminal-vt "3"))))))
+                           ;; put hyprlock here eventually
+
+                           (simple-service 'add-nonguix-substitutes
+                                           guix-service-type
+                                           (guix-extension
+                                            (substitute-urls
+                                             (append (list "https://substitutes.nonguix.org")
+                                                     %default-substitute-urls))
+                                            (authorized-keys
+                                             (append (list (plain-file "nonguix.pub"
+                                                                       "(public-key (ecc (curve Ed25519) (q #C1FD53E5D4CE971933EC50C9F307AE2171A2D3B52C804642A7A35F84F3A4EA98#)))"))
+                                                     %default-authorized-guix-keys))))
+                           polkit-wheel-service
+
+                           (service network-manager-service-type
+                                    (network-manager-configuration
+                                     (vpn-plugins
+                                      (list network-manager-openvpn))))
+                           (service wpa-supplicant-service-type)
+                           (service modem-manager-service-type)
+                           (service bluetooth-service-type
+                                    (bluetooth-configuration
+                                     (auto-enable? #t)))
+                           (service usb-modeswitch-service-type)
+
+                           (service avahi-service-type)
+                           (service udisks-service-type)
+                           (service upower-service-type)
+                           (service cups-pk-helper-service-type)
+                           (service geoclue-service-type)
+                           (service polkit-service-type)
+                           (service dbus-root-service-type)
+                           fontconfig-file-system-service
+
+                           (service thermald-service-type)
+                           (service tlp-service-type
+                                    (tlp-configuration
+                                     (cpu-boost-on-ac? #t)
+                                     (wifi-pwr-on-bat? #t)))
+
+               ;; realtime mode
+               (service pam-limits-service-type
+                        (list
+                         (pam-limits-entry "@realtime" 'both 'rtprio 99)
+                         (pam-limits-entry "@realtime" 'both 'nice -19)
+                         (pam-limits-entry "@realtime" 'both 'memlock 'unlimited)))
+
+               ;; v4l2loopback module for virtual cameras
+               ;; see: https://stackoverflow.com/a/66072635
+               ;;           https://github.com/umlaeute/v4l2loopback
+               (service kernel-module-loader-service-type '("v4l2loopback"))
+               (simple-service 'v4l2loopback-config etc-service-type
+                               (list `("modprobe.d/v4l2loopback.conf"
+                                       ,(plain-file "v4l2loopback.conf"
+                                                    "options v4l2loopback devices=1 video_nr=2 exclusive_caps=1 card_label=\"OBS Virtual Camera\""))))
+
+
+               ;; docker
+               (service containerd-service-type)
+               (service docker-service-type)
+               (service libvirt-service-type
+                        (libvirt-configuration
+                         (unix-sock-group "libvirt")
+                         (tls-port "16555")))
+
+               ;; ssh
+               (service openssh-service-type
+                        (openssh-configuration
+                         (openssh openssh-sans-x)
+                         (port-number 2222)))
+
+               ;; printing/scanning
+               (service sane-service-type)
+               (service cups-service-type
+                        (cups-configuration
+                         (web-interface? #t)
+                         (extensions
+                          (list cups-filters))))
+
+               ;; x11 socket directory
+               (service x11-socket-directory-service-type)
+
+               ;; sync system clock
+               (service ntp-service-type)
+               
+               ;; udev rules 
+               (udev-rules-service 'pipewire-add-udev-rules pipewire)
+               (udev-rules-service 'brightnessctl-udev-rules brightnessctl)
+               (simple-service 'mtp udev-service-type (list libmtp)) ; udev rules for non-root user access for mpt devices
+
+               ;; enable nix build service
+               (service nix-service-type)
+
+               ;; system cron jobs
+               (simple-service 'system-cron-jobs
+                               mcron-service-type
+                               (list
+                                ;; run `guix gc' 5 minutes after midnight every day.
+                                ;; clean up generations older than 2 months and free
+                                ;; at least 10g of space.
+                                #~(job "5 0 * * *" "guix gc -d 2m -F 10G"))))
                      (modify-services %base-services
                                       (delete login-service-type)
                                       (delete mingetty-service-type)
                                       (delete console-font-service-type))))))
-
-base-operating-system
+core-operating-system
