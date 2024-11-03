9c10c1216b7ee53cfd718a831f3ad813b75fb7a6
polhuang <paulleehuang@gmail.com>
2024-11-03 04:40:41 -0600

    Add and enable nix service

--- polterguix/systems/akhetaten.scm
+++ polterguix/systems/akhetaten.scm
@@ -28,54 +28,22 @@
    #:use-module (nongnu packages mozilla)
    #:use-module (gnu services)
    #:use-module (guix gexp)
-   #:use-module (gnu home services shells)
    #:use-module (polterguix packages hyprland)
    #:use-module (polterguix packages desktop)
    #:use-module (polterguix packages security)
    #:use-module (polterguix packages cli)
+   #:use-module (gnu home services shells)
    #:use-module (polterguix systems core-system))
-
-;; (use-modules (gnu home)
-;;              (gnu packages)
-;;              (gnu packages admin)
-;;              (gnu packages xdisorg)
-;;              (gnu packages emacs-xyz)
-;;              (gnu packages emacs)
-;;              (gnu packages gnupg)
-;;              (gnu packages librewolf)
-;;              (gnu packages video)
-;;              (gnu packages fonts)
-;;              (gnu packages rust)
-;;              (gnu packages rust-apps)
-;;              (gnu packages gnupg)
-;;              (gnu packages vim)
-;;              (gnu packages mail)
-;;              (gnu packages password-utils)
-;;              (gnu packages wm)
-;;              (gnu packages autotools)
-;;              (gnu packages web-browsers)
-;;              (gnu packages terminals)
-;;              (gnu packages package-management)
-;;              (nongnu packages mozilla)
-;;              (gnu services)
-;;              (guix gexp)
-;;              (gnu home services shells)
-;;              (polterguix packages hyprland)
-;;              (polterguix packages desktop)
-;;              (polterguix packages security)
-;;              (polterguix packages cli))
 
 (define system
   (operating-system
    (inherit core-operating-system)
-   (host-name "akhetaten")
-   ))
+   (host-name "akhetaten")))
 
 (define home
   (home-environment
- ;; Below is the list of packages that will show up in your ;; Home profile, under ~/.guix-home/profile.
-   (packages (list pinentry ;;new
-                   pinentry-emacs ;new
+   (packages (list pinentry
+                   pinentry-emacs
                    emacs-next-pgtk-xwidgets
                    emacs-guix
                    emacs-jinx
@@ -117,25 +85,23 @@
                    zsh-syntax-highlighting
                    sh-z
                    fzf
-                   ))
-   ;; Below is the list of Home services.  To search for available
-   ;; services, run 'guix home search KEYWORD' in a terminal.
+                   nix))
    (services
-   (list (service home-zsh-service-type
-                  (home-zsh-configuration
-                   (zshrc (list 
-                                (mixed-text-file "zsh-autosuggestions"
-                                                 "source $HOME/.guix-home/profile/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh")
-                                (mixed-text-file "zsh-syntax-highlighting"
-                                                 "source $HOME/.guix-home/profile/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh")
-                                (mixed-text-file "zsh-completions"
-                                                 "fpath=($HOME/.guix-home/share/zsh/site-functions $fpath)")
-                                (local-file
-                                 "/home/pol/polterguix/files/.zshrc" "zshrc")
-))
-                   (zprofile (list (local-file
-                                    "/home/pol/polterguix/files/.zprofile"
-                                    "zprofile")))))))))
+    (list (service home-zsh-service-type
+                   (home-zsh-configuration
+                    (zshrc (list 
+                            (mixed-text-file "zsh-autosuggestions"
+                                             "source $HOME/.guix-home/profile/share/zsh/plugins/zsh-autosuggestions/zsh-autosuggestions.zsh")
+                            (mixed-text-file "zsh-syntax-highlighting"
+                                             "source $HOME/.guix-home/profile/share/zsh/plugins/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh")
+                            (mixed-text-file "zsh-completions"
+                                             "fpath=($HOME/.guix-home/share/zsh/site-functions $fpath)")
+                            (local-file
+                             "/home/pol/polterguix/files/.zshrc" "zshrc")
+                            ))
+                    (zprofile (list (local-file
+                                     "/home/pol/polterguix/files/.zprofile"
+                                     "zprofile")))))))))
 
 (if (equal? (getenv "GUIX_TARGET") "home")
     home
