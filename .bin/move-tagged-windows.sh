d89bb8310eaa064a62be615ae6ef72fea430bdaa
Paul Huang <paulleehuang@gmail.com>
2025-02-17 22:33:04 -0600

    Add nineveh-specific dimensions for lefty special workspace

--- .bin/move-tagged-windows.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env sh
-
-pids=$(hyprctl clients | grep -A5 'class: .*kitty.*' | grep 'pid' | awk '{print $2}')
-
-# Move each "kitty" client to the "shell" workspace
-for pid in $pids; do
-  hyprctl dispatch movetoworkspace special:shell, pid:$pid
-done
