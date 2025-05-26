e89b0224b2da55c69384415003e803fa20141bb6
Paul Huang <paulleehuang@gmail.com>
2025-05-26 16:42:49 -0500

    Correct backup shell script

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -4,12 +4,12 @@
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
 RESTIC_REPOSITORY="rclone:protondrive:backups/nineveh"
-RESTIC_PASSWORD_FILE="$(pass show restic)"
-LOG_FILE="/path/to/backup.log"  # Update this to your desired log file path
+RESTIC_PASSWORD="$(pass show restic)"
+LOG_FILE="/home/$USERNAME/backup.log"  # Update this to your desired log file path
 
 # Export necessary variables for Restic
 export RESTIC_REPOSITORY
-export RESTIC_PASSWORD_FILE
+export RESTIC_PASSWORD
 
 # Start the backup
 echo "$(date): Starting backup" | tee -a "$LOG_FILE"
@@ -17,6 +17,12 @@ echo "$(date): Starting backup" | tee -a "$LOG_FILE"
 restic -r "$RESTIC_REPOSITORY" backup \
     /home/"$USERNAME" \
     /etc \
-    /usr/local/bin | tee -a "$LOG_FILE"
+    /usr/local/bin \
+    --exclude="/home/$USERNAME/.cache" \
+    --exclude="/home/$USERNAME/.local/share/Trash" \
+    --exclude="/home/$USERNAME/Downloads" \
+    --exclude="/home/$USERNAME/.thumbnails" \
+    --exclude="*.iso" \
+    --exclude="*.tmp" | tee -a "$LOG_FILE"                            
 
 echo "$(date): Backup completed" | tee -a "$LOG_FILE"
