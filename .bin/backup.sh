416a2d790f5189fd6f72826f53d3c1c29eaee8ae
Paul Huang <paulleehuang@gmail.com>
2025-05-27 06:50:52 -0500

    Minor changes

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -6,7 +6,7 @@ RESTIC_REPOSITORY="rclone:proton:backups/$HOSTNAME"
 RESTIC_PASSWORD="$(pass show restic)"
 LOG_FILE="/home/$USERNAME/backup.log"
 
-# Exclude patterns
+# exclude patterns
 EXCLUDED_PATTERNS="
     --exclude '.cache/**' 
     --exclude '.local/share/Trash/**' 
@@ -20,15 +20,15 @@ export RESTIC_PASSWORD
 
 echo "$(date): Starting Google Drive sync" | tee -a "$LOG_FILE"
 
-# Sync using rclone with excluded patterns
-rclone sync -vv --progress /home/$USERNAME/Documents gdrive:/Backups/$USERNAME/Documents $EXCLUDED_PATTERNS | tee -a "$LOG_FILE"
-rclone sync -vv --progress /home/$USERNAME/Documents proton:/backups/$USERNAME/Documents $EXCLUDED_PATTERNS | tee -a "$LOG_FILE"
-rclone sync -vv --progress /home/$USERNAME/Downloads proton:/backups/$HOSTNAME/Downloads $EXCLUDED_PATTERNS | tee -a "$LOG_FILE"
+# sync using rclone with excluded patterns
+rclone sync -vv -P /home/$USERNAME/Documents gdrive:/Backups/$HOSTNAME/Documents $EXCLUDED_PATTERNS | tee -a "$LOG_FILE" # google drive Backups folder capitalized
+rclone sync -vv -P /home/$USERNAME/Documents proton:/backups/$HOSTNAME/Documents $EXCLUDED_PATTERNS | tee -a "$LOG_FILE" # proton drive backups folder not capitalized
+rclone sync -vv -P /home/$USERNAME/Downloads proton:/backups/$HOSTNAME/Downloads $EXCLUDED_PATTERNS | tee -a "$LOG_FILE"
 
 echo "$(date): Starting Restic backup" | tee -a "$LOG_FILE"
 
-# Backup to Proton Drive using restic with excludes
-restic -r "$RESTIC_REPOSITORY" backup -vv \
+# backup to proton drive using restic with excludes
+restic -r -vv "$RESTIC_REPOSITORY" backup \
     /home/"$USERNAME" \
     /etc \
     $EXCLUDED_PATTERNS | tee -a "$LOG_FILE"
