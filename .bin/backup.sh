ea0b75be2c6a9825696ff4a0e69642b92687b169
Paul Huang <paulleehuang@gmail.com>
2025-05-28 05:46:41 -0500

    Remove unnecessary Google Drive syncs (only Documents); add more exclusions to Proton backup

--- .bin/backup.sh
+++ .bin/backup.sh
@@ -3,34 +3,42 @@
 USERNAME=$(whoami)
 HOSTNAME=$(hostname)
 RESTIC_REPOSITORY="rclone:proton:backups/$HOSTNAME"
-RESTIC_PASSWORD="$(pass show restic)"
 LOG_FILE="/home/$USERNAME/backup.log"
 
-# exclude patterns
-EXCLUDED_PATTERNS="
-    --exclude '.cache/**' 
-    --exclude '.local/share/Trash/**' 
-    --exclude '.thumbnails/**' 
-    --exclude '*.iso' 
-    --exclude '*.tmp'
-"
-
 export RESTIC_REPOSITORY
-export RESTIC_PASSWORD
 
 echo "$(date): Starting Google Drive sync" | tee -a "$LOG_FILE"
 
-# sync using rclone with excluded patterns
-rclone sync -vv -P /home/$USERNAME/Documents gdrive:/Backups/$HOSTNAME/Documents $EXCLUDED_PATTERNS | tee -a "$LOG_FILE" # google drive Backups folder capitalized
-rclone sync -vv -P /home/$USERNAME/Documents proton:/backups/$HOSTNAME/Documents $EXCLUDED_PATTERNS | tee -a "$LOG_FILE" # proton drive backups folder not capitalized
-rclone sync -vv -P /home/$USERNAME/Downloads proton:/backups/$HOSTNAME/Downloads $EXCLUDED_PATTERNS | tee -a "$LOG_FILE"
+# sync using rclone with excludes
+rclone sync -vv -P /home/$USERNAME/Documents gdrive:/Backups/$HOSTNAME/Documents \
+       --exclude "*.tmp" | tee -a "$LOG_FILE"
 
 echo "$(date): Starting Restic backup" | tee -a "$LOG_FILE"
 
 # backup to proton drive using restic with excludes
-restic -r -vv "$RESTIC_REPOSITORY" backup \
-    /home/"$USERNAME" \
-    /etc \
-    $EXCLUDED_PATTERNS | tee -a "$LOG_FILE"
-
+restic -r "$RESTIC_REPOSITORY" \
+       --password-file ~/.restic_password \
+       backup -vv /home/"$USERNAME" \
+       --exclude ".cache/**" \
+       --exclude "**/*[Cc]ache*/**" \
+       --exclude ".local/share/**" \
+       --exclude "**.lock" \
+       --exclude ".thumbnails/**" \
+       --exclude "**/.DS_Store" \
+       --exclude "*.iso" \
+       --exclude "*.tmp" \
+       --exclude ".config/discord/**" \
+       --exclude ".config/BraveSoftware/**" \
+       --exclude ".config/Cursor/**" \
+       --exclude ".config/google-chrome/**" \
+       --exclude ".config/libreoffice/**" \
+       --exclude ".config/Slack/**" \
+       --exclude ".config/Windsurf/**" \
+       --exclude ".npm/**" \
+       --exclude ".mozilla/**" \
+       --exclude ".zoom/**" \
+       --exclude ".rustup/**" \
+       --exclude ".cargo/**" \
+       --exclude ".cursor/**" \
+       /etc \
 echo "$(date): Backup completed" | tee -a "$LOG_FILE"
