a76b0c1423e294f508fd968d9466562c87559aa9
Paul Huang <paulleehuang@gmail.com>
2025-06-25 06:29:29 -0500

    Add network-manager and network-manager-applet to guix-managed packages

--- polternix/home/default.nix
+++ polternix/home/default.nix
@@ -1,18 +1,22 @@
 { config, pkgs, ... }:
 
 {
-  home.username = "pol";
-  home.homeDirectory = "/home/pol";
+  home.username = "polhuang";
+  home.homeDirectory = "/home/polhuang";
   home.stateVersion = "23.11";
 
   home.packages = [
     pkgs._1password-gui-beta
     pkgs.activitywatch
     pkgs.google-chrome
+    pkgs.morewaita-icon-theme
+    pkgs.mu
+    # pkgs.networkmanagerapplet
     pkgs.protonmail-bridge-gui
-    pkgs.protonvpn-gui
+    # pkgs.protonvpn-gui
     pkgs.slack
-    pkgs.whatsapp-for-linux
+    pkgs.ticktick
+    pkgs.winbox
   ];
 
   programs.home-manager.enable = true;
